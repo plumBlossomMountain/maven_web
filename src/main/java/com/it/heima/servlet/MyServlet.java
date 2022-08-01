96c843529f3d7a25ee0f474e73e637890cdea320
plumBlossomMountain <90032132+plumBlossomMountain@users.noreply.github.com>
2022-08-01 10:26:04 +0800

    pull test

--- src/main/java/com/it/heima/servlet/MyServlet.java
+++ src/main/java/com/it/heima/servlet/MyServlet.java
@@ -20,6 +20,7 @@ public class MyServlet extends HttpServlet {
         System.out.println("hot-fix test ");
         System.out.println("push test ");
         System.out.println("push second ");
+        System.out.println("pull test");
         doGet(request, response);
     }
 
